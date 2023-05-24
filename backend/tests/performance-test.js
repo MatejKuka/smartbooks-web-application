6a5276bef2991993d57d009ffcc2788dcba043fe
Matej Kuka <mate1778@easv365.dk>
2023-05-24 18:43:37 +0200

    -t ftsdrtrtredtred

--- backend/tests/performance-test.js
+++ backend/tests/performance-test.js
@@ -2,7 +2,7 @@ import { sleep } from 'k6';
 import http from 'k6/http';
 
 export const options = {
-    duration: '5',
+    duration: '5s',
     vus: 10,
     thresholds: {
         http_req_duration: ['p(95)<700'],
